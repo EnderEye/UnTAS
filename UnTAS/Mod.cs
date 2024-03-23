420b7bbb2d95906fc78aaceec1d6de518e363a1f
EnderEye <endereye0@yahoo.com>
2024-03-23 13:39:11 -0700

    Added version checker, a reset speed key, changed the pause key, and moved the text.

--- UnTAS/Mod.cs
+++ UnTAS/Mod.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
 using MelonLoader;
 using UnTAS;
+using System.Net;
 
-[assembly: MelonInfo(typeof(Mod), "UnTAS", "0.0.1", "EnderEye")]
+[assembly: MelonInfo(typeof(Mod), "UnTAS", "0.0.2", "EnderEye")]
 
 namespace UnTAS
 {
@@ -12,22 +13,45 @@ namespace UnTAS
         public static float baseTimeScale = 1f;
         public static bool frozen = false;
 
+        public static string version = "0.0.2";
+        public static bool updated;
+
         public static KeyCode SlowKey = KeyCode.Minus;
         public static KeyCode FastKey = KeyCode.Equals;
-        public static KeyCode PauseKey = KeyCode.P;
+        public static KeyCode PauseKey = KeyCode.R;
+        public static KeyCode ResetKey = KeyCode.M;
 
         public static void DrawFrozenText()
         {
-            GUI.Label(new Rect(20, 20, 1000, 200), "<b><color=white><size=100>Frozen</size></color></b>");
+            GUI.Label(new Rect((Screen.width / 2) - 150, (Screen.height / 2) - 150, 1000, 200), "<b><color=white><size=100>Frozen</size></color></b>");
         }
         
         public static void DrawWatermark()
         {
-            GUI.Label(new Rect(Screen.width - 225, Screen.height - 50, 1000, 200), "<b><color=white><size=30>UnTAS v0.0.1</size></color></b>");
+            string addition;
+            if(updated)
+            {
+                addition = "updated";
+            }
+            else
+            {
+                addition = "out of date";
+            }
+            GUI.Label(new Rect((Screen.width / 2) - 150, Screen.height - 60, 1000, 200), "<b><color=white><size=30>UnTAS v" + version + " (" + addition + ")</size></color></b>");
         }
 
-        public override void OnLateInitializeMelon()
+        public override void OnInitializeMelon()
         {
+            WebClient wb = new WebClient();
+            string latest = wb.DownloadString("https://raw.githubusercontent.com/EnderEye/UnTAS/master/version");
+            if(latest == version + "\n")
+            {
+                updated = true;
+            }
+            else
+            {
+                updated = false;
+            }
             MelonEvents.OnGUI.Subscribe(DrawWatermark, 101);
         }
 
@@ -52,6 +76,11 @@ namespace UnTAS
             if(Input.GetKeyDown(PauseKey))
             {
                 ToggleFreeze();
+            }
+
+            if(Input.GetKeyDown(ResetKey))
+            {
+                baseTimeScale = 1f;
             }
 
             Time.timeScale = baseTimeScale;
